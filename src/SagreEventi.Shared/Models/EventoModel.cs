190249e9b053307454d4eb41aeca8399466d374d
Angelo Pirola <angelo@aepservice.it>
2022-07-05 22:38:06 +0200

    SagreEventi.Shared (WIP)

---
+++ src/SagreEventi.Shared/Models/EventoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SagreEventi.Shared.Models;
+public class EventoModel
+{
+    public int Id { get; set; }
+    public string NomeEvento { get; set; }
+    public string DescrizioneEvento { get; set; }
+    public string CittaEvento { get; set; }
+    public DateTime DataOraEvento { get; set; }
+    public bool EventoConcluso { get; set; }
+    public DateTime DataOraUltimaModifica { get; set; }
+}
